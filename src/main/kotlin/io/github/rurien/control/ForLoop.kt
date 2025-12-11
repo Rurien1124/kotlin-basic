2cb0e7a04f7c389090743bf9b4c192134ab26a58
Rurien <a01092603909@gmail.com>
2025-12-11 12:23:07 +0900

    Add ForLoop

---
+++ src/main/kotlin/io/github/rurien/control/ForLoop.kt
@@ -0,0 +1,34 @@
+package io.github.rurien.control
+
+/** For 반복문 */
+class ForLoop {
+
+  companion object {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+      // 범위
+      for (i in 1..10) {
+        print("$i ")
+      }
+
+      println()
+
+      // 내림차
+      for (i in 10 downTo 1) {
+        print("$i ")
+      }
+
+      println()
+
+      // 건너뛰기
+      for (i in 1..11 step 3) {
+        print("$i ")
+      }
+
+      println()
+      println((10 downTo 1).joinToString(", "))
+      println((10 downTo 1).joinToString("|") { num -> "[$num]" })
+    }
+  }
+}
